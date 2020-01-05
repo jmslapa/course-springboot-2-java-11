9994f4f692894eb9a8431529b994e5c09338ac0e
jmslapa <lapeta96@gmail.com>
2020-01-05 00:02:56 -0300

    Camada de serviço, registro de componentes

--- src/main/java/com/coursejava/webservices/repositories/UserRepository.java
+++ src/main/java/com/coursejava/webservices/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.coursejava.webservices.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.coursejava.webservices.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 	
 }
