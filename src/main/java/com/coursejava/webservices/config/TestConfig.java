accea78897cd7fe2153989acb5115ccc047d2417
jmslapa <lapeta96@gmail.com>
2020-01-05 17:20:19 -0300

    Entidade Orderitem. Associação M2M com dados extras -  Parte 2

--- src/main/java/com/coursejava/webservices/config/TestConfig.java
+++ src/main/java/com/coursejava/webservices/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.coursejava.webservices.entities.Category;
 import com.coursejava.webservices.entities.Order;
+import com.coursejava.webservices.entities.OrderItem;
 import com.coursejava.webservices.entities.Product;
 import com.coursejava.webservices.entities.User;
 import com.coursejava.webservices.entities.enums.OrderStatus;
 import com.coursejava.webservices.repositories.CategoryRepository;
+import com.coursejava.webservices.repositories.OrderItemRepository;
 import com.coursejava.webservices.repositories.OrderRepository;
 import com.coursejava.webservices.repositories.ProductRepository;
 import com.coursejava.webservices.repositories.UserRepository;
@@ -30,6 +32,8 @@ public class TestConfig implements CommandLineRunner{
 	private CategoryRepository categoryRepository;
 	@Autowired
 	private ProductRepository productRepository;
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -65,5 +69,12 @@ public class TestConfig implements CommandLineRunner{
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
 }
